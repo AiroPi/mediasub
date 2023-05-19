c1f962a87446f54faea3892ebf00f68a80110c69
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-05-19 14:00:39 +0200

    Update example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mediasub
-Allow you to sub to different media (new books, new anime, etc...) by building a source that implement some rules.
+
+Allow you to sub to different media (new books, new anime, etc...) by building a source that implement some methods.
+
+## Sources implementations examples
+
+Sources implementation for scan-vf.net, manga-scan.ws, scanmanga-vf.net, scan-fr.org :
+
+https://pastebin.com/wNay08D3
--- examples/basic.py
+++ examples/basic.py
@@ -1,49 +1,27 @@
 import asyncio
-import pathlib
+import logging
 
-from mediasub import Content, MediaSub
-from mediasub.builtins.sources import ScanVFDotNet
-from mediasub.content import ContentType
+from mediasub import MediaSub
+from mediasub.models import Chapter, MangaSource
 
-media_sub = MediaSub("./history.json")
+logger = logging.getLogger(__name__)
 
-
-# By using this decorator, the "new_content" function will now be called when there is new content.
-@media_sub.sub_to(ScanVFDotNet())
-async def new_content(content: Content) -> None:
-    # you can download some contents if available
-    if content.downloadable:
-        dl = await content.download()
-        with open(dl[0], "wb") as f:
-            f.write(dl[1].read())
-
-    # MANGA_SCAN content can often be downloaded page by page
-    elif content.fragmented_downloadable and content.content_type == ContentType.MANGA_SCAN:
-        path = pathlib.Path(content.name)
-        path.mkdir(exist_ok=True)
-
-        async for page in content.fragmented_download():
-            fragment_path = path / page[0]
-            with fragment_path.open("rb") as f:
-                f.write(page[1].read())
+media_sub = MediaSub("./history.sqlite")
 
 
-# In fact, download methods depend on the source used. They can be defined or not.
-# Because here we know that ScanVFDotNet implement the fragmented_downloadable method by returning the scan page by page
-# we could use the method directly without any check.
-# fragmented_downloadable & downloadable can be used if you use multiple source for the same callback.
+MangaDex: MangaSource  # not implemented, only used as example
 
 
-# Then a better example would be :
-@media_sub.sub_to(ScanVFDotNet())
-async def scan_vf_new_content(content: Content) -> None:
-    path = pathlib.Path(content.name)
-    path.mkdir(exist_ok=True)
+@media_sub.sub_to(MangaSource, MangaDex())
+async def on_new_chapter(source: MangaSource, chapter: Chapter) -> None:
+    print(chapter.manga.name, "#", chapter.number, f"({chapter.name})")
 
-    async for page in content.fragmented_download():
-        fragment_path = path / page[0]
-        with fragment_path.open("rb") as f:
-            f.write(page[1].read())
+    if source.supports_download:
+        pages = await source.get_pages(chapter)
+        for page in pages:
+            filename, bytes_img = await source.download(page)
+            with open(f"page_{page.number}_{filename}", "wb") as f:
+                f.write(bytes_img.read())
 
 
 asyncio.run(media_sub.start())
