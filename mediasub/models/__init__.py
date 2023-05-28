8e69c58d13a445a6cd1b763d2bf8675ee54531be
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-05-28 14:50:19 +0200

    expose *SupportsDownload classes

--- mediasub/models/__init__.py
+++ mediasub/models/__init__.py
@@ -1,7 +1,9 @@
 from .anime import Anime as Anime
 from .anime import AnimeSource as AnimeSource
+from .anime import AnimeSupportsDownload as AnimeSupportsDownload
 from .anime import Episode as Episode
 from .manga import Chapter as Chapter
 from .manga import Manga as Manga
 from .manga import MangaSource as MangaSource
+from .manga import MangaSupportsDownload as MangaSupportsDownload
 from .manga import Page as Page
