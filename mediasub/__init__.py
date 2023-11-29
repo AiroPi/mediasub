67bc54489c8cf9cc35949dc337e5c2e173aae547
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-11-29 13:29:11 +0100

    bump version to 1.3.0

--- mediasub/__init__.py
+++ mediasub/__init__.py
@@ -1,6 +1,5 @@
 from .core import MediaSub as MediaSub
 from .errors import SourceDown as SourceDown
-from .source import Identifiable as Identifiable
-from .source import Source as Source
+from .source import Identifiable as Identifiable, Source as Source
 
-__version__ = "1.2.2"
+__version__ = "1.3.0"
