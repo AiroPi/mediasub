64ffb10beb18affa9a3dd266c23b17aa0177d618
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-07-05 19:53:34 +0200

    feat: more root exposed objects

--- mediasub/__init__.py
+++ mediasub/__init__.py
@@ -1,5 +1,10 @@
 from .core import MediaSub as MediaSub
 from .errors import SourceDown as SourceDown
-from .source import Identifiable as Identifiable, Source as Source
+from .source import (
+    Identifiable as Identifiable,
+    PubsubSource as PubsubSource,
+    PullSource as PullSource,
+    Source as Source,
+)
 
-__version__ = "1.3.0"
+__version__ = "2.0.0b1"
